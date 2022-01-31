dc699c3f53a33e25847d77d08d5677e845a3549e
manucarenjot <carenjotje@gmail.com>
2022-01-31 16:17:29 +0100

    revision

--- profil.php
+++ profil.php
@@ -2,6 +2,7 @@
 session_start();
 setcookie('username',$_POST['name'], time() + 3600 * 24, '/', 'localhost', true, true);
 $_SESSION['username'] = $_POST['name'];
+$username = $_POST['name'];
 
 foreach ($_COOKIE AS $cookieName => $cookieValue) {
     echo "$cookieName => $cookieValue <br>";
@@ -29,4 +30,19 @@ if (isset($_POST['logout'])) {
 
 if (isset($_GET['logout'])) {
     echo 'Vous avez été deconnecté';
+    ?>
+<form method="post" action="profil.php">
+    <button name="log">Reconnection</button>
+</form>
+<?php
+}
+
+if (isset($_POST['log'])) {
+
+    header('Location: ?relog=0');
+}
+
+if (isset($_GET['relog'])) {
+    $_SESSION['username'] = $username;
+    echo 'Vous avez été reconnecté' . $_SESSION['username'];
 }
